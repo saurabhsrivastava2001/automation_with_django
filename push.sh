975d1c83dfbc7b86ef739f8060fdbc4e8e1e0043
saurabhsrivastava2001 <srisaurabh2001@gmail.com>
2025-08-02 03:24:12 +0530

    github workflow configuration

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 git add .
-git commit -m" created the models.py and  insertdata management command to insert the 1 data or the list of data signing off day 1(1:8:25)"
+git commit -m" github workflow configuration"
 git branch -M main
 git push origin main
